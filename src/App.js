8fee88f38adda65a5538a2e29a90e1fae3138368
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-04-06 23:21:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import { YoutubeData } from "./YoutubeData";
+import "./App.css";
+// JSX: Javascript XML
+// ES6
+// Babel
+// curly braces{}
+function Feature() {
+   return (
+      <div className="feature">
+         <img src="" alt="" className="feature-image" />
+         <h3 class="feature-title">Title2</h3>
+         <p class="feature-desc">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores rerum aliquam quae molestias
+            dolor magnam provident non facere commodi sint!
+         </p>
+      </div>
+   );
 }
+// parent component
+function App() {
+   // const numbers = [1, 2, 3, 4, 5];
+   // const double = numbers.map((item) => item * 2);
+   // console.log(double);
+   return (
+      <div className="youtube-list">
+         {YoutubeData.map((item, index) => {
+            let newClass = "";
+            if (index === 1) newClass = "abc";
+            {
+               /* const newClass = index === 1 ? "abc" : ""; */
+            }
+            return (
+               <YoutubeItem
+                  key={item.id}
+                  image={item.image}
+                  avatar={item.avatar || item.image}
+                  title={item.title}
+                  author={item.author}
+                  className={newClass}
+               ></YoutubeItem>
+            );
+         })}
+      </div>
+   );
+}
+// Props -> Properties
+function YoutubeItem(props) {
+   return (
+      <div
+         className={`youtube-item ${props.className}`}
+         //  style={{
+         //     height: "250px",
+         //  }}
+      >
+         <div className="youtube-image">
+            <img
+               src={props.image}
+               // alt=""
+               // style={{
+               //    display: "block",
+               //    maxWidth: "100%",
+               //    width: "100%",
+               //    height: "100%",
+               //    objectFit: "cover",
+               // }}
+            />
+         </div>
+         <div className="youtube-footer">
+            <img src={props.avatar} alt="" className="youtube-avatar" />
+            <div className="youtube-info">
+               <h3 className="youtube-title">{props.title || "This is example of title"}</h3>
+               <h4 className="youtube-author">{props.author || "This is example of author name"}</h4>
+            </div>
+         </div>
+      </div>
+   );
+}
+// Rendering list
 
 export default App;
---
+++ src/YoutubeData.js
@@ -0,0 +1,26 @@
+export const YoutubeData = [
+   {
+      id: 1,
+      title: "This is example of title1",
+      author: "This is example of author1",
+      image: "https://images.unsplash.com/photo-1680462155145-0152dc0646dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      avatar:
+         "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+   },
+   {
+      id: 2,
+      title: "This is example of title2",
+      author: "This is example of author2",
+      image: "https://images.unsplash.com/photo-1496979149951-4fd28cf221e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      avatar:
+         "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+   },
+   {
+      id: 3,
+      title: "This is example of title3",
+      author: "This is example of author3",
+      image: "https://plus.unsplash.com/premium_photo-1673971700988-346588461fa7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+      avatar:
+         "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+   },
+];
